d93b1faa0a02973c31c853d49b5835c79da7ffdf
ottlseo <dotsi@ewhain.net>
2023-04-23 13:14:43 +0900

    test: KCP 결제 테스트

--- components/main/Teaser.tsx
+++ components/main/Teaser.tsx
@@ -1,5 +1,41 @@
 import React from 'react';
 import { css, styled } from 'stitches.config';
+import Button from '../common/Button';
+
+declare const window: typeof globalThis & {
+  IMP: any;
+};
+const requestPay = () => {
+  const IMP = window.IMP;
+  IMP.init('imp23026546');
+
+  IMP.request_pay(
+    {
+      // param
+      pg: `kcp.T0000`,
+      pay_method: 'card',
+      // m_redirect_url: 'http://https://2023.pycon.kr/payments/complete',
+      merchant_uid: 'ORD20180131-00000',
+      name: '노르웨이 회전 의자',
+      amount: 100,
+      buyer_email: 'gildong@gmail.com',
+      buyer_name: '홍길동',
+      buyer_tel: '010-4242-4242', // 필수
+      buyer_addr: '서울특별시 강남구 신사동',
+      buyer_postcode: '01181',
+    },
+    (rsp: any) => {
+      // callback
+      if (rsp.success) {
+        console.log('결제 성공');
+        console.log(rsp);
+      } else {
+        console.log('결제 실패');
+        console.log(rsp);
+      }
+    }
+  );
+};
 
 const TextBox = styled('div', {
   fontSize: '80px',
@@ -43,11 +79,16 @@ const Location = styled('div', {
 });
 const Teaser = () => {
   return (
-    <TextBox>
-      <Title>{'다시, \n우리, \n파이썬'}</Title>
-      <Date>2023.08.11-13</Date>
-      <Location>COEX 그랜드볼룸 & 아셈볼룸</Location>
-    </TextBox>
+    <>
+      <TextBox>
+        <Button size="big" onClick={requestPay}>
+          결제하기
+        </Button>
+        <Title>{'다시, \n우리, \n파이썬'}</Title>
+        <Date>2023.08.11-13</Date>
+        <Location>COEX 그랜드볼룸 & 아셈볼룸</Location>
+      </TextBox>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Teaser from '@/components/main/Teaser';
 import type { NextPage } from 'next';
 import SeoHeader from '@/components/layout/SeoHeader';
 import { Routes } from '@/constants/routes';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +11,9 @@ const Home: NextPage = () => {
         title={Routes.HOME.title}
         description="파이콘 한국 2023: 8월 11~13일 코엑스"
       />
+      <Head>
+        <script src="https://cdn.iamport.kr/v1/iamport.js"></script>
+      </Head>
       <Teaser />
     </>
   );
